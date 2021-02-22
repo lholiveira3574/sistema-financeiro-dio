d63f5d044b807380e558e56a369c04c4063a2537
Luiz Henrique <luizhenrique3574@gmail.com>
2021-02-22 07:36:36 -0300

    Criação do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sistema_financeiro_dio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
